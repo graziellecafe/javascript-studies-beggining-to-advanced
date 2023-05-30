d67b673745d230497ba32bd202a16c48c8056286
Grazielle Café <graziellecafe@MacBook-Pro-de-Grazielle.local>
2023-05-30 10:41:53 -0300

    feat: sobre funcoes e arrow function

---
+++ funcoes/02-escopo-de-uma-funcao/script.js
@@ -0,0 +1,9 @@
+let n = 10; 
+
+const numero = function() {
+    let n = 25;
+    console.log(n);
+}
+
+console.log(n); 
+numero(); 
---
+++ funcoes/03-arrow-functions/script.js
@@ -0,0 +1,24 @@
+ let consoleTeste = () => {
+    console.log("Teste"); 
+ }
+
+ consoleTeste(); 
+
+ // 
+
+ let soma = (a,b) => {
+    return a + b; 
+ }
+
+ console.log(soma(3,4)); 
+
+ //
+
+ const raizQuadrada = (x) => { 
+    return x * x; 
+   }
+  
+   const raizQuadrada2 = n => n * n; 
+  
+   console.log(raizQuadrada(2)); 
+   console.log(raizQuadrada2(4));
