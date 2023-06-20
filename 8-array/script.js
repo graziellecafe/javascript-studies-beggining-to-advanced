80fb467dd4bd1a95ea206158866b4e646112740a
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2023-06-20 12:57:46 -0300

    feat: sobre arrays e exemplos

---
+++ 8-array/script.js
@@ -0,0 +1,24 @@
+let array = [1, 4, 5, 6, 7];
+console.log(array);
+
+let nomes = ["Matheus", "Grazielle", "Café"]; 
+console.log(nomes);
+
+let booleanos = [true, false];
+console.log(booleanos);
+
+let misturado = [1, "Grazielle", true]; 
+console.log(misturado); 
+
+// mais sobre arrays 
+console.log(array[1]);
+console.log(nomes[0]); 
+
+// propriedades
+let numeros = [1,3,5,6,7,8,9]; 
+console.log(numeros.length); 
+
+// métodos
+let marca = "Nike";
+console.log(marca.toUpperCase()); 
+
