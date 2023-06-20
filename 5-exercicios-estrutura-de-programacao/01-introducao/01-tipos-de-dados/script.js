0d4d393d6c970d567928f7f3b8c8eaa72cda5032
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2023-06-20 12:52:07 -0300

    feat: eexercícios de estrutura de programação

---
+++ 5-exercicios-estrutura-de-programacao/01-introducao/01-tipos-de-dados/script.js
@@ -0,0 +1,9 @@
+// Armazene em valores em variáveis com cada um dos tipos de dados vistos
+
+const nome = "Grazielle"; 
+const idade = 29; 
+const confirma = true; 
+
+console.log(typeof nome); 
+console.log(typeof idade); 
+console.log(typeof confirma); 
---
+++ 5-exercicios-estrutura-de-programacao/02-if/script.js
@@ -0,0 +1,13 @@
+/* Cria uma estrutura if e verifica a entrada na balada, se tiver mais de 18 anos 
+- Armazenar a idade em uma variável let 
+- Insira uma instrução console.log("Pode entrar") caso a pessoa tenha mais que 18 anos
+*/ 
+
+let idade = 19; 
+
+if(idade => 18){ 
+    console.log("Pode entrar, tem idade suficiente"); 
+
+} else {
+    console.log("Não tem idade suficiente.")
+}
